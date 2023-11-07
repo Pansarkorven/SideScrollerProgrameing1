c0bd71f7a5864bead5aba8fbf2af475c022f0290
Viktor Myrhill <viktor.myrhill@learnet.se>
2023-11-07 10:56:45 +0100

    Enemy prefab and script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float MovmentSpeedPerSecond = 10.0f;
+
+    public Rigidbody2D myRigidBody = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        
+        //copy character velocity from rigidbofy
+        Vector3 characterVelocity = myRigidBody.velocity;
+        
+        //sett character velocity to 0
+        characterVelocity.x = 0;
+        
+        //add velocity in character right direktion
+        characterVelocity += MovmentSpeedPerSecond * transform.right.normalized;
+        
+        //copy modfied velocity bakc onto rigidbody
+        myRigidBody.velocity = characterVelocity;
+    }
+}
